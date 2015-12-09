b33a96764d03b9a5645266f197a5776bb6b6142a
Khorshed Alam <thekhorshed.alam@gmail.com>
2015-12-09 14:41:45 +0600

    Remove sensor property to remove warning in console

--- cmb-field-map.php
+++ cmb-field-map.php
@@ -77,7 +77,7 @@ class PW_CMB2_Field_Google_Maps {
 	 * Enqueue scripts and styles
 	 */
 	public function setup_admin_scripts() {
-		wp_register_script( 'pw-google-maps-api', '//maps.googleapis.com/maps/api/js?sensor=false&libraries=places', null, null );
+		wp_register_script( 'pw-google-maps-api', '//maps.googleapis.com/maps/api/js?libraries=places', null, null );
 		wp_enqueue_script( 'pw-google-maps', plugins_url( 'js/script.js', __FILE__ ), array( 'pw-google-maps-api' ), self::VERSION );
 		wp_enqueue_style( 'pw-google-maps', plugins_url( 'css/style.css', __FILE__ ), array(), self::VERSION );
 	}
