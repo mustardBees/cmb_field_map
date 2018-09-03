65cc11482c6b283acbf85cc9eb7fd239ad450a43
Phil Wylie <philw@iweb.co.uk>
2018-09-03 15:59:33 +0100

    Add a default filter to set the API key from the ‘PW_GOOGLE_API_KEY’ constant, for example, from a wp-config.php file.

--- cmb-field-map.php
+++ cmb-field-map.php
@@ -26,13 +26,19 @@ class PW_CMB2_Field_Google_Maps {
 	public function __construct() {
 		add_filter( 'cmb2_render_pw_map', array( $this, 'render_pw_map' ), 10, 5 );
 		add_filter( 'cmb2_sanitize_pw_map', array( $this, 'sanitize_pw_map' ), 10, 4 );
+		add_filter( 'pw_google_api_key', array( $this, 'google_api_key_constant' ) );
 	}
 
 	/**
 	 * Render field
 	 */
 	public function render_pw_map( $field, $field_escaped_value, $field_object_id, $field_object_type, $field_type_object ) {
-		$api_key = apply_filters( 'cmb2-pw-map-api-key', $field->args( 'api_key' ) );
+
+		// Get the Google API key from the field's parameters
+		$api_key = $field->args( 'api_key' );
+
+		// Allow a custom hook to specify the key
+		$api_key = apply_filters( 'pw_google_api_key', $api_key );
 
 		$this->setup_admin_scripts( $api_key );
 
@@ -82,6 +88,23 @@ class PW_CMB2_Field_Google_Maps {
 		wp_register_script( 'pw-google-maps-api', "https://maps.googleapis.com/maps/api/js?key={$api_key}&libraries=places", null, null );
 		wp_enqueue_script( 'pw-google-maps', plugins_url( 'js/script.js', __FILE__ ), array( 'pw-google-maps-api', 'jquery' ), self::VERSION );
 		wp_enqueue_style( 'pw-google-maps', plugins_url( 'css/style.css', __FILE__ ), array(), self::VERSION );
+	}
+
+	/**
+	 * Default filter to return a Google API key constant if defined
+	 */
+	public function google_api_key_constant( $google_api_key = null ) {
+
+		// Allow the field's 'api_key' parameter or a custom hook to take precedence
+		if ( ! empty( $google_api_key ) ) {
+			return $google_api_key;
+		}
+
+		if ( defined( 'PW_GOOGLE_API_KEY' ) ) {
+			$google_api_key = PW_GOOGLE_API_KEY;
+		}
+
+		return $google_api_key;
 	}
 }
 $pw_cmb2_field_google_maps = new PW_CMB2_Field_Google_Maps();
